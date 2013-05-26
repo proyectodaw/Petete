c2666d0a15f5b637bd1d57166df4edc242a35068
proyectodaw <rypsk@hotmail.com>
2013-05-26 19:10:15 +0200

    Añadir cambios

--- Usuario.php
+++ Usuario.php
@@ -4,7 +4,8 @@ class Usuario {
     public function annadirUsuario($array, $conexion) {
         if ($resultado = $conexion->query("insert into usuarios values('$array[0]','$array[1]',
                 '$array[2]','$array[3]','$array[4]','$array[5]','$array[6]','$array[7]','$array[8]',
-                    '$array[9]','$array[10]','$array[11]','$array[12]','$array[13]')")) {
+                    '$array[9]','$array[10]','$array[11]','$array[12]','$array[13]','$array[14]'
+                        ,'$array[15]','$array[16]','$array[17]','$array[18]','$array[19]')")) {
             return $array;
         } else {
             return false;
